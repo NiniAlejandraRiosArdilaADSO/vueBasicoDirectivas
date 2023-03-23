d0c56b97f239c354189e80b886a5872d1ba78013
NiniAlejandraRiosArdilaADSO <ninialejandra0424@gmail.com>
2023-03-22 23:04:11 -0500

    feat: :sparkles: v-html

--- component/my-component.js
+++ component/my-component.js
@@ -1,9 +1,9 @@
 export default{
     data(){
         return {
-            basico: "Apolo",
-            intermedio: "Artemis",
-            avanzado: "Sputnik"
+            basico: `<strong style="color:green;">Apolo</strong>`,
+            intermedio: `<strong style="color:orange;">Artemis</strong>`,
+            avanzado: `<strong style="color:blue;">Artemis</strong>`
         }
     }
 }
