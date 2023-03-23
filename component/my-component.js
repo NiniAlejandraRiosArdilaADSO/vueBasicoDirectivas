27446ab9e158b714699f3ed003b3840384e05e3b
NiniAlejandraRiosArdilaADSO <ninialejandra0424@gmail.com>
2023-03-22 22:57:40 -0500

    Revert "feat: :sparkles: v-html"
    This reverts commit c10c192e786974add4d9861fda2efa0cfbacd68d.

--- component/my-component.js
+++ component/my-component.js
@@ -1,9 +1,9 @@
 export default{
     data(){
         return {
-            basico:`<strong style="color:green;">Apolo</strong>`,
-            intermedio:`<strong style="color:orange;">Artemis</strong>`,
-            avanzado:`<strong style="color:blue;">Spurnik</strong>`,
+            basico: "Apolo",
+            intermedio: "Artemis",
+            avanzado: "Sputnik"
         }
     }
 }
