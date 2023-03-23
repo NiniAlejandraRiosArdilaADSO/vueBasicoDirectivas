7ccaa9f98b69a2abe3a4cd90046629d81957a768
NiniAlejandraRiosArdilaADSO <ninialejandra0424@gmail.com>
2023-03-23 00:08:43 -0500

    feat: :sparkles: v-for

--- component/my-component.js
+++ component/my-component.js
@@ -1,11 +1,11 @@
 export default{
     data(){
         return {
-            mensaje: `<em>Apolo</em>`,
-            mensaje2: `<mark>Artemis</mark>`,
-            mensaje3: `<u>Sputnik</u>`,
-            mensaje4: `NO CUMPLE LA CONDICION`,
-            opcion: null,
+            lista: [
+                "apolo",
+                "artemis",
+                "sputnik"
+            ]
         }
     }
 }
