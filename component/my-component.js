741d2938a3c18a595f00f26f19df328ca640b427
NiniAlejandraRiosArdilaADSO <ninialejandra0424@gmail.com>
2023-03-24 12:12:15 -0500

    feat: :sparkles: Directivas avanzadas
    v_bind

--- component/my-component.js
+++ component/my-component.js
@@ -1,11 +1,8 @@
 export default{
     data(){
         return {
-            lista: [
-                "apolo",
-                "artemis",
-                "sputnik"
-            ]
+            imagen:"https://lenguajejs.com/vuejs/componentes/composition-api/options-api-composition-api.png",
+            texto: "Diferencias entre vue2 y vue3"
         }
     }
 }
