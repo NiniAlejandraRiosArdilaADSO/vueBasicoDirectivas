f18dd2541d9dc8c443a77c92b7fc777c426e19cc
NiniAlejandraRiosArdilaADSO <ninialejandra0424@gmail.com>
2023-03-24 12:24:24 -0500

    feat: :sparkles: dinamico
    no me salio

--- component/my-component.js
+++ component/my-component.js
@@ -1,8 +1,10 @@
 export default{
     data(){
         return {
-            imagen:"https://lenguajejs.com/vuejs/componentes/composition-api/options-api-composition-api.png",
-            texto: "Diferencias entre vue2 y vue3"
+            atributo: "src",
+            imagen: "https://lenguajejs.com/vuejs/componentes/composition-api/options-api-composition-api.png",
+            alt: "alt",
+            texto: "direncias entre vue2 y vue3"
         }
     }
 }
